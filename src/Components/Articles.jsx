23db951f8953492b76cc8e4aec8b8ab82906502f
Peter Zak Austin <peterzaustin@icloud.com>
2024-12-18 14:05:54 +0000

    add sorting

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -3,28 +3,61 @@ import { getArticles } from "../api";
 import ArticleCard from "./ArticleCard";
 import useQueryParams from "./QueryParams";
 
-
 const Articles = () => {
   const [articles, setArticles] = useState([]);
   const [topicMessage, setTopicMessage] = useState("");
-  const queryParams = useQueryParams()
-  const topic = queryParams.get("topic")
+  const queryParams = useQueryParams();
+  const topic = queryParams.get("topic");
+  const validSorting = ["created_at", "author", "comment_count", "votes"];
+  const [sorting, setSorting] = useState("created_at");
+  const [order, setOrder] = useState('desc');
 
   useEffect(() => {
-    getArticles(topic).then((articles) => {
-      if (articles.length > 0){
-      setArticles(articles);
-      setTopicMessage("")
+    getArticles(topic, sorting, order).then((articles) => {
+      if (articles.length > 0) {
+        setArticles(articles);
+        setTopicMessage("");
       } else {
         setArticles([]);
-        setTopicMessage(`There are currently no articles with the topic of ${topic}, why don't you write one?`)
+        setTopicMessage(
+          `There are currently no articles with the topic of ${topic}, why don't you write one?`
+        );
       }
     });
-  }, [topic]);
+  }, [topic, sorting, order]);
 
   return (
     <>
       <h2>Articles</h2>
+      <div className="article-sorting">
+        <label>Sort by</label>
+        <select
+          id="sort-select"
+          value={sorting}
+          onChange={(e) => setSorting(e.target.value)}
+        >
+          {validSorting.map((sortingMethod, index) => (
+            <option key={index} value={sortingMethod}>
+              {sortingMethod}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="order-select">
+      <label>Order by</label>
+        <select
+          id="sort-select"
+          value={order}
+          onChange={(e) => setOrder(e.target.value)}
+        >
+            <option value={'DESC'}>
+              descending
+            </option>
+            <option value={'ASC'}>
+              ascending
+            </option>
+        </select>
+      </div>
       <div className="article-grid">
         {topicMessage ? (
           <p>{topicMessage}</p>
--- src/api.js
+++ src/api.js
@@ -4,20 +4,20 @@ const api = axios.create({
   baseURL: "https://news-api-5gcc.onrender.com/api",
 });
 
-const getArticles = (query) => {
-  if (query){
-  return api.get(`/articles?topic=${query}`).then(({ data }) => {
-    const { articles } = data;
-    return articles;
-  });
-} else {
-  return api.get("/articles").then(({ data }) => {
-    const { articles } = data;
-    return articles;
-  });
-
-}
-};
+const getArticles = (query, sorting, order) => {
+    const params = [];
+  
+    if (query) params.push(`topic=${query}`);
+    if (sorting) params.push(`sort_by=${sorting}`);
+    if (order) params.push(`order=${order}`);
+  
+    const url = `/articles${params.length ? `?${params.join('&')}` : ''}`;
+  
+    return api.get(url).then(({ data }) => {
+      const { articles } = data;
+      return articles;
+    });
+  };
 
 const getUsers = () => {
   return api.get("/users").then(({ data }) => {
@@ -63,8 +63,8 @@ const postComment = (article_id, username, commentBody) => {
       body: commentBody,
     })
     .then(({ data }) => {
-      const {comment} = data
-      return comment
+      const { comment } = data;
+      return comment;
     })
     .catch((err) => {
       throw new Error(
@@ -74,8 +74,8 @@ const postComment = (article_id, username, commentBody) => {
 };
 
 const deleteComment = (comment_id) => {
-  return api.delete(`/comments/${comment_id}`)
-}
+  return api.delete(`/comments/${comment_id}`);
+};
 
 export {
   getArticles,
@@ -85,5 +85,5 @@ export {
   updateArticleVotes,
   postComment,
   getUsers,
-  deleteComment
+  deleteComment,
 };
